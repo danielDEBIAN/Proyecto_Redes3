76ed7c7685912994d99a1d1a11daef73aa5e6176
Daniel Espinoza <espinoza.bautista.daniel246@gmail.com>
2023-06-15 10:58:28 -0600

    ACL,DNS,NAT y DHCP implementados, cambios interfaz

--- python_proyecto/main.py
+++ python_proyecto/main.py
@@ -1,28 +1,149 @@
 # Importamos librerias
 import PySimpleGUI as sg
 import paramiko
-import telnetlib
 import time
 
 # Funcion para ventana de mostrar configuracion
-def show_configuration():
+def session(usuario, password):
     # Definimos el tema de la ventana
     sg.theme('DarkTeal6')
 
     # Definimos el layout de la ventana
     layout_configuration = [
-        [sg.Text('IP del router', font=('Arial', 15))],
-        [sg.Input(key='-INPUT-IP-', font=('Arial', 12))],
-        [sg.Text('Usuario', font=('Arial', 15))],
-        [sg.Input(key='-INPUT-USER-', font=('Arial', 12))],
-        [sg.Text('Password', font=('Arial', 15))],
-        [sg.Input(key='-INPUT-PASS-', password_char='*', font=('Arial', 12))],
-        [sg.Text('¿Que configuracion deseas ver?', font=('Arial', 15))],
-        [sg.DropDown(["Resumen","Interfaces", "Ruteo", "ACL", "DHCP", "NAT", "DNS"], key='-DROPDOWN-', font=('Arial', 12))],
+        [sg.Text('¿Como deseas conectarte?', font=('Consolas', 13))],
         [sg.Text("", size=(1, 1))],
-        [sg.Button('Conectarse', font=('Arial', 12))],
-        [sg.Output(size=(80,20), font=('Arial', 12), key="-OUTPUT-")],
-        [sg.Button('Regresar', font=('Arial', 10))]
+        [sg.Button('Conectarse por IP', font=('Consolas', 10))],
+        [sg.Button('Conectarse por Router', font=('Consolas', 10))],
+        [sg.Text("", size=(1, 1))],
+        [sg.Button('Cerrar Sesion', font=('Consolas', 8))]
+    ]
+
+    # Creamos la ventana
+    window_session = sg.Window('Mostrar configuraciones', layout_configuration, element_justification='center')
+
+    # Generamos el loop para que se procesen los eventos y se obtengan valores
+    while True:
+        event, values = window_session.read()
+        if event == 'Conectarse por IP':
+            window_session.close()
+            show_connection_ip(usuario, password)
+            break
+        if event == 'Conectarse por Router':
+            window_session.close()
+            show_connection_router(usuario, password)
+            break
+        if event == 'Cerrar Sesion' or event == sg.WIN_CLOSED: # Definimos si se cierra la ventana se termina el evento o si se selecciona salir
+            window_session.close()
+            main()
+            break
+
+    window_session.close()
+
+def show_connection_ip(usuario, password):
+    # Definimos el tema de la ventana
+    sg.theme('DarkTeal6')
+
+    # Definimos el layout de la ventana
+    layout_configuration = [
+        [sg.Text('IP del router', font=('Consolas', 13))],
+        [sg.Input(key='-INPUT-IP-', font=('Consolas', 10))],
+        [sg.Text('¿Que configuracion deseas ver?', font=('Consolas', 13))],
+        [sg.DropDown(["Resumen","Interfaces", "Ruteo", "ACL", "DHCP", "NAT", "DNS"], key='-DROPDOWN-', font=('Consolas', 10))],
+        [sg.Text("", size=(1, 1))],
+        [sg.Button('Conectarse', font=('Consolas', 10))],
+        [sg.Output(size=(80,10), font=('Consolas', 10), key="-OUTPUT-")],
+        [sg.Button('Regresar', font=('Consolas', 8))]
+    ]
+
+    # Creamos la ventana
+    window_configuration = sg.Window('Mostrar configuraciones', layout_configuration, element_justification='center')
+
+    # Generamos el loop para que se procesen los eventos y se obtengan valores
+    while True:
+        event, values = window_configuration.read()
+        if event == 'Conectarse':
+            if values['-INPUT-IP-'] == "":
+                sg.popup('Error', 'Digite una IP')
+            if values['-DROPDOWN-'] == "Resumen":
+                try:
+                    window_configuration['-OUTPUT-'].update("")
+                    print('Conectando ...')
+                    output = get_configuration(values['-INPUT-IP-'], usuario, password)
+                    print(output)
+                except Exception as e:
+                    print('Error:', str(e))
+            elif values['-DROPDOWN-'] == "Interfaces":
+                try:
+                    window_configuration['-OUTPUT-'].update("")
+                    print('Conectando ...')
+                    output = get_interfaces(values['-INPUT-IP-'], usuario, password)
+                    print(output)
+                except Exception as e:
+                    print('Error:', str(e))
+            elif values['-DROPDOWN-'] == "Ruteo":
+                try:
+                    window_configuration['-OUTPUT-'].update("")
+                    print('Conectando ...')
+                    output = get_route(values['-INPUT-IP-'], usuario, password)
+                    print(output)
+                except Exception as e:
+                    print('Error:', str(e))
+            elif values['-DROPDOWN-'] == '':
+                sg.popup('Error', 'Seleccione una opcion')
+            
+        if event == 'Regresar' or event == sg.WIN_CLOSED: # Definimos si se cierra la ventana se termina el evento o si se selecciona salir
+            window_configuration.close()
+            session(usuario, password)
+            break
+
+    window_configuration.close()
+
+def show_connection_router(usuario, password):
+    # Definimos el tema de la ventana
+    sg.theme('DarkTeal6')
+
+    # Definimos el layout de la ventana
+    layout_configuration = [
+        [sg.Text('Selecciona el router', font=('Consolas', 13))],
+        [sg.DropDown(["R1","R2", "R3", "R4", "R5"], key='-DROPDOWN-', font=('Consolas', 10))],
+        [sg.Text("", size=(1, 1))],
+        [sg.Button('Conectarse', font=('Consolas', 10))],
+        [sg.Button('Regresar', font=('Consolas', 8))]
+    ]
+
+    # Creamos la ventana
+    window_configuration = sg.Window('Mostrar configuraciones', layout_configuration, element_justification='center')
+
+    # Generamos el loop para que se procesen los eventos y se obtengan valores
+    while True:
+        event, values = window_configuration.read()
+        if event == 'Conectarse':
+            if values['-DROPDOWN-'] == "R1":
+                window_configuration.close()
+                connection_router('10.1.1.1', usuario, password)
+                break
+            elif values['-DROPDOWN-'] == "":
+                sg.popup('Error', 'Seleccione una opcion')
+
+        if event == 'Regresar' or event == sg.WIN_CLOSED: # Definimos si se cierra la ventana se termina el evento o si se selecciona salir
+            window_configuration.close()
+            session(usuario, password)
+            break
+
+    window_configuration.close()
+
+def connection_router(ip, usuario, password):
+    # Definimos el tema de la ventana
+    sg.theme('DarkTeal6')
+
+    # Definimos el layout de la ventana
+    layout_configuration = [
+        [sg.Text('¿Que configuracion deseas ver?', font=('Consolas', 13))],
+        [sg.DropDown(["Resumen","Interfaces", "Ruteo", "ACL", "DHCP", "NAT", "DNS"], key='-DROPDOWN-', font=('Consolas', 10))],
+        [sg.Text("", size=(1, 1))],
+        [sg.Button('Mostrar', font=('Consolas', 10))],
+        [sg.Output(size=(80,10), font=('Consolas', 10), key="-OUTPUT-")],
+        [sg.Button('Regresar', font=('Consolas', 8))]
     ]
 
     # Creamos la ventana
@@ -36,34 +157,69 @@ def show_configuration():
                 try:
                     window_configuration['-OUTPUT-'].update("")
                     print('Conectando ...')
-                    output = get_configuration(values['-INPUT-IP-'], values['-INPUT-USER-'], values['-INPUT-PASS-'])
+                    output = get_configuration(ip, usuario, password)
                     print(output)
                 except Exception as e:
-                    print('Error:', str(e))
+                    print('Error: ', str(e))
             elif values['-DROPDOWN-'] == "Interfaces":
                 try:
                     window_configuration['-OUTPUT-'].update("")
                     print('Conectando ...')
-                    output = get_interfaces(values['-INPUT-IP-'], values['-INPUT-USER-'], values['-INPUT-PASS-'])
+                    output = get_interfaces(ip, usuario, password)
                     print(output)
                 except Exception as e:
-                    print('Error:', str(e))
+                    print('Error: ', str(e))
             elif values['-DROPDOWN-'] == "Ruteo":
                 try:
                     window_configuration['-OUTPUT-'].update("")
                     print('Conectando ...')
-                    output = get_route(values['-INPUT-IP-'], values['-INPUT-USER-'], values['-INPUT-PASS-'])
+                    output = get_route(ip, usuario, password)
                     print(output)
                 except Exception as e:
-                    print('Error:', str(e))
-            
+                    print('Error: ', str(e))
+            elif values['-DROPDOWN-'] == "ACL":
+                try:
+                    window_configuration['-OUTPUT-'].update("")
+                    print('Conectando ...')
+                    output = get_lists(ip, usuario, password)
+                    print(output)
+                except Exception as e:
+                    print('Error: ', str(e))
+            elif values['-DROPDOWN-'] == "DHCP":
+                try:
+                    window_configuration['-OUTPUT-'].update("")
+                    print('Conectando ...')
+                    output = get_dhcp(ip, usuario, password)
+                    print(output)
+                except Exception as e:
+                    print('Error: ', str(e))
+            elif values['-DROPDOWN-'] == "NAT":
+                try:
+                    window_configuration['-OUTPUT-'].update("")
+                    print('Conectando ...')
+                    output = get_nat(ip, usuario, password)
+                    print(output)
+                except Exception as e:
+                    print('Error: ', str(e))
+            elif values['-DROPDOWN-'] == "DNS":
+                try:
+                    window_configuration['-OUTPUT-'].update("")
+                    print('Conectando ...')
+                    output = get_dns(ip, usuario, password)
+                    print(output)
+                except Exception as e:
+                    print('Error: ', str(e))
+            elif values['-DROPDOWN-'] == "":
+                sg.popup('Error', 'Seleccione una opcion')
+
         if event == 'Regresar' or event == sg.WIN_CLOSED: # Definimos si se cierra la ventana se termina el evento o si se selecciona salir
             window_configuration.close()
-            main()
+            session(usuario, password)
             break
 
     window_configuration.close()
 
+# Funcion para obtener el resumen de la configuracion
 def get_configuration(ip, user, password):
     try:
         # Realizamos la configuracion de SSH
@@ -85,6 +241,7 @@ def get_configuration(ip, user, password):
     except Exception as e:
         return "Error en la conexion:\n"+str(e)
 
+# Funcion para obtener la informacion de ruteo
 def get_route(ip, user, password):
     try:
         # Realizamos la configuracion de SSH
@@ -105,7 +262,8 @@ def get_route(ip, user, password):
         return "Conexion exitosa!\n"+output
     except Exception as e:
         return "Error en la conexion:\n"+str(e)
-    
+
+# Funcion para obtener el resumen de las interfaces
 def get_interfaces(ip, user, password):
     try:
         # Realizamos la configuracion de SSH
@@ -127,85 +285,97 @@ def get_interfaces(ip, user, password):
     except Exception as e:
         return "Error en la conexion:\n"+str(e)
 
-# Funcion para ventana de mostrar enrutamiento
-def routing():
-    # Definimos el tema de la ventana
-    sg.theme('DarkTeal6')
+# Funcion para obtener las listas de acceso
+def get_lists(ip, user, password):
+    try:
+        # Realizamos la configuracion de SSH
+        ssh_client = paramiko.SSHClient()
+        ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh_client.connect(hostname=ip, port='22', username=user, password=password)
 
-    # Definimos el layout de la ventana
-    layout_routing = [
-        [sg.Text('Mostrar configuracion', justification='center')],
-        [sg.Text('¿Que router quieres observar su enrutamiento?', justification='center')],
-        [sg.Button('Router1'), sg.Button('Router2')],
-        [sg.Button('Router3'), sg.Button('Router4')],
-        [sg.Button('RouterPC'), sg.Button('Toda la red')],
-        [sg.Button('Regresar')]
-    ]
+        # Mandamos los comandos que queremos
+        shell = ssh_client.invoke_shell()
+        time.sleep(1)
+        shell.send('enable\n')
+        time.sleep(1)
+        shell.send('enable_password\n')
+        time.sleep(1)
+        shell.send('show access-lists\n')
+        time.sleep(5) 
+        output = shell.recv(65535).decode('ascii')
+        return "Conexion exitosa!\n"+output
+    except Exception as e:
+        return "Error en la conexion:\n"+str(e)
+    
+# Funcion para obtener la informacion de DHCP
+def get_dhcp(ip, user, password):
+    try:
+        # Realizamos la configuracion de SSH
+        ssh_client = paramiko.SSHClient()
+        ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh_client.connect(hostname=ip, port='22', username=user, password=password)
 
-    # Creamos la ventana
-    window_routing = sg.Window('Enrutamiento', layout_routing)
+        # Mandamos los comandos que queremos
+        shell = ssh_client.invoke_shell()
+        time.sleep(1)
+        shell.send('enable\n')
+        time.sleep(1)
+        shell.send('enable_password\n')
+        time.sleep(1)
+        shell.send('show running-config | include dhcp pool\n')
+        time.sleep(5) 
+        output = shell.recv(65535).decode('ascii')
+        return "Conexion exitosa!\n"+output
+    except Exception as e:
+        return "Error en la conexion:\n"+str(e)
 
-    # Generamos el loop para que se procesen los eventos y se obtengan valores
-    while True:
-        event, values = window_routing.read()
-        if event == 'Regresar' or event == sg.WIN_CLOSED: # Definimos si se cierra la ventana se termina el evento o si se selecciona salir
-            window_routing.close()
-            main()
-            break
-    window_routing.close()
+# Funcion para obtener la informacion de NAT
+def get_nat(ip, user, password):
+    try:
+        # Realizamos la configuracion de SSH
+        ssh_client = paramiko.SSHClient()
+        ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh_client.connect(hostname=ip, port='22', username=user, password=password)
 
-# Funcion para conectarse a routers
-def connect():
-    # Definimos el tema de la ventana
-    sg.theme('DarkTeal6')
+        # Mandamos los comandos que queremos
+        shell = ssh_client.invoke_shell()
+        time.sleep(1)
+        shell.send('enable\n')
+        time.sleep(1)
+        shell.send('enable_password\n')
+        time.sleep(1)
+        shell.send('show running-config | include nat\n')
+        time.sleep(1)
+        shell.send('show ip nat translations\n')
+        time.sleep(5) 
+        output = shell.recv(65535).decode('ascii')
+        return "Conexion exitosa!\n"+output
+    except Exception as e:
+        return "Error en la conexion:\n"+str(e)
+    
+# Funcion para obtener la informacion de DNS
+def get_dns(ip, user, password):
+    try:
+        # Realizamos la configuracion de SSH
+        ssh_client = paramiko.SSHClient()
+        ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh_client.connect(hostname=ip, port='22', username=user, password=password)
 
-    # Definimos el layout de la ventana
-    layout_connect = [
-        [sg.Text('Conectarse a routers', justification='center')],
-        [sg.Text('¿A que router quieres acceder?', justification='center')],
-        [sg.Button('Router1'), sg.Button('Router2')],
-        [sg.Button('Router3'), sg.Button('Router4')],
-        [sg.Button('RouterPC')],
-        [sg.Button('Regresar')]
-    ]
-
-    # Creamos la ventana
-    window_connect = sg.Window('Conectarse', layout_connect)
-
-    # Generamos el loop para que se procesen los eventos y se obtengan valores
-    while True:
-        event, values = window_connect.read()
-        if event == 'Regresar' or event == sg.WIN_CLOSED: # Definimos si se cierra la ventana se termina el evento o si se selecciona salir
-            window_connect.close()
-            main()
-            break
-    window_connect.close()
-
-# Funcion para mostrar varios
-def varios():
-    # Definimos el tema de la ventana
-    sg.theme('DarkTeal6')
-
-    # Definimos el layout de la ventana
-    layout_varios = [
-        [sg.Text('Varios', justification='center')],
-        [sg.Text('¿Que configuracion quieres observar?', justification='center')],
-        [sg.Button('ACL'), sg.Button('DHCP'), sg.Button('NAT')],
-        [sg.Button('DNS'), sg.Button('VPN'), sg.Button('SNMP')],
-        [sg.Button('Regresar')]
-    ]
-
-    # Creamos la ventana
-    window_varios = sg.Window('Configuracion', layout_varios)
-
-    # Generamos el loop para que se procesen los eventos y se obtengan valores
-    while True:
-        event, values = window_varios.read()
-        if event == 'Regresar' or event == sg.WIN_CLOSED: # Definimos si se cierra la ventana se termina el evento o si se selecciona salir
-            window_varios.close()
-            main()
-            break
-    window_varios.close()
+        # Mandamos los comandos que queremos
+        shell = ssh_client.invoke_shell()
+        time.sleep(1)
+        shell.send('enable\n')
+        time.sleep(1)
+        shell.send('enable_password\n')
+        time.sleep(1)
+        shell.send('show running-config | include dns\n')
+        time.sleep(1)
+        shell.send('show dns statistics\n')
+        time.sleep(5) 
+        output = shell.recv(65535).decode('ascii')
+        return "Conexion exitosa!\n"+output
+    except Exception as e:
+        return "Error en la conexion:\n"+str(e)
 
 # Funcion main
 def main():
@@ -214,11 +384,11 @@ def main():
 
     # Definimos el layout de la ventana
     layout_main = [
-        [sg.Text('Configuracion de Routers', justification='center', font=('Arial', 18))],
-        [sg.Text('¿Que deseas hacer?', justification='center', font=('Arial', 15))],
-        [sg.Button('Mostrar configuracion', font=('Arial', 12)), sg.Button('Mostrar enrutamiento', font=('Arial', 12))],
-        [sg.Button('Conectarse a routers', font=('Arial', 12)), sg.Button('Varios', font=('Arial', 12))],
-        [sg.Button('Salir', font=('Arial', 10))]
+        [sg.Text('Usuario', font=('Consolas', 15))],
+        [sg.Input(key='-INPUT-USER-', font=('Consolas', 10))],
+        [sg.Text('Contraseña', font=('Consolas', 15))],
+        [sg.Input(key='-INPUT-PASS-', password_char='*', font=('Consolas', 10))],
+        [sg.Button('Iniciar sesion', font=('Consolas', 10)), sg.Button('Salir', font=('Consolas', 10))]
     ]
 
     # Creamos la ventana
@@ -228,19 +398,14 @@ def main():
     while True:
         event, values = window_main.read()
         if event == 'Salir' or event == sg.WIN_CLOSED:
+            window_main.close()
             break
-        if event == 'Mostrar configuracion':
+        if event == 'Iniciar sesion' and values['-INPUT-USER-'] == 'cisco' and values['-INPUT-PASS-'] == 'cisco':
             window_main.close()
-            show_configuration()
-        if event == 'Mostrar enrutamiento':
-            window_main.close()
-            routing()
-        if event == 'Conectarse a routers':
-            window_main.close()
-            connect()
-        if event == 'Varios':
-            window_main.close()
-            varios()
+            session(values['-INPUT-USER-'], values['-INPUT-PASS-'])
+            break
+        else:
+            sg.popup('Error', 'Revisa tus credenciales')
     window_main.close()
 
 # Ejecutamos el codigo de main
