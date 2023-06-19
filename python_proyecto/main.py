10ecf95358b96c75df16c31a519fa66551e13e54
Daniel Espinoza <espinoza.bautista.daniel246@gmail.com>
2023-06-18 21:08:34 -0600

    Refactor en codigo

--- python_proyecto/main.py
+++ python_proyecto/main.py
@@ -2,6 +2,7 @@
 import PySimpleGUI as sg
 import paramiko
 import time
+import ipaddress
 
 # Funcion para ventana de mostrar configuracion
 def session(usuario, password):
@@ -30,7 +31,7 @@ def session(usuario, password):
             break
         if event == 'Conectarse por Router':
             window_session.close()
-            show_connection_router(usuario, password)
+            connection_router(usuario, password)
             break
         if event == 'Cerrar Sesion' or event == sg.WIN_CLOSED: # Definimos si se cierra la ventana se termina el evento o si se selecciona salir
             window_session.close()
@@ -62,26 +63,20 @@ def show_connection_ip(usuario, password):
     while True:
         event, values = window_configuration.read()
         if event == 'Conectarse':
+            # Comprobacion de que se selecciono algo
             if values['-INPUT-IP-'] == "":
                 sg.popup('Error', 'Digite una IP')
-            if values['-DROPDOWN-'] == "Interfaces":
-                try:
-                    window_configuration['-OUTPUT-'].update("")
-                    print('Conectando ...')
-                    output = get_interfaces(values['-INPUT-IP-'], usuario, password)
-                    print(output)
-                except Exception as e:
-                    print('Error:', str(e))
-            elif values['-DROPDOWN-'] == "Ruteo":
-                try:
-                    window_configuration['-OUTPUT-'].update("")
-                    print('Conectando ...')
-                    output = get_route(values['-INPUT-IP-'], usuario, password)
-                    print(output)
-                except Exception as e:
-                    print('Error:', str(e))
             elif values['-DROPDOWN-'] == '':
                 sg.popup('Error', 'Seleccione una opcion')
+            elif values['-INPUT-IP-'] == "" and values['-DROPDOWN-'] == '':
+                sg.popup('Error', 'Digite una IP y seleccione una configuracion')
+            elif ip_valid(values['-INPUT-IP-']) == False:
+                sg.popup('Error', 'IP no valida, digite una nueva')
+            else: # Obtencion de la informacion
+                window_configuration['-OUTPUT-'].update("")
+                print('Conectando ...')
+                output = get_configuration(values['-INPUT-IP-'], usuario, password, values['-DROPDOWN-'])
+                print(output)
             
         if event == 'Regresar' or event == sg.WIN_CLOSED: # Definimos si se cierra la ventana se termina el evento o si se selecciona salir
             window_configuration.close()
@@ -90,64 +85,25 @@ def show_connection_ip(usuario, password):
 
     window_configuration.close()
 
-def show_connection_router(usuario, password):
-    # Definimos el tema de la ventana
-    sg.theme('DarkTeal6')
+# Funcion para comprobar que la IP es valida
+def ip_valid(ip_str):
+    try:
+        ipaddress.ip_address(ip_str)
+        return True
+    except ValueError:
+        return False
 
-    # Definimos el layout de la ventana
-    layout_configuration = [
-        [sg.Text('Selecciona el router', font=('Consolas', 13))],
-        [sg.DropDown(["R1","R2", "R3", "R4", "R5"], key='-DROPDOWN-', font=('Consolas', 10))],
-        [sg.Text("", size=(1, 1))],
-        [sg.Button('Conectarse', font=('Consolas', 10))],
-        [sg.Button('Regresar', font=('Consolas', 8))]
-    ]
-
-    # Creamos la ventana
-    window_configuration = sg.Window('Mostrar configuraciones', layout_configuration, element_justification='center')
-
-    # Generamos el loop para que se procesen los eventos y se obtengan valores
-    while True:
-        event, values = window_configuration.read()
-        if event == 'Conectarse':
-            if values['-DROPDOWN-'] == "R1":
-                window_configuration.close()
-                connection_router('10.1.1.1', usuario, password)
-                break
-            if values['-DROPDOWN-'] == "R2":
-                window_configuration.close()
-                connection_router('10.1.4.1', usuario, password)
-                break
-            if values['-DROPDOWN-'] == "R3":
-                window_configuration.close()
-                connection_router('10.1.3.1', usuario, password)
-                break
-            if values['-DROPDOWN-'] == "R4":
-                window_configuration.close()
-                connection_router('10.1.3.2', usuario, password)
-                break
-            if values['-DROPDOWN-'] == "R5":
-                window_configuration.close()
-                connection_router('192.172.10.1', usuario, password)
-                break
-            elif values['-DROPDOWN-'] == "":
-                sg.popup('Error', 'Seleccione una opcion')
-
-        if event == 'Regresar' or event == sg.WIN_CLOSED: # Definimos si se cierra la ventana se termina el evento o si se selecciona salir
-            window_configuration.close()
-            session(usuario, password)
-            break
-
-    window_configuration.close()
-
+# Ventana para conectarse por router
 def connection_router(ip, usuario, password):
     # Definimos el tema de la ventana
     sg.theme('DarkTeal6')
 
     # Definimos el layout de la ventana
     layout_configuration = [
+        [sg.Text('Selecciona el router', font=('Consolas', 13))],
+        [sg.DropDown(["R1","R2", "R3", "R4", "R5"], key='-DROPDOWN-R-', font=('Consolas', 10))],
         [sg.Text('¿Que configuracion deseas ver?', font=('Consolas', 13))],
-        [sg.DropDown(["Interfaces", "Ruteo", "ACL", "DHCP", "NAT", "DNS"], key='-DROPDOWN-', font=('Consolas', 10))],
+        [sg.DropDown(["Interfaces", "Ruteo", "ACL", "DHCP", "NAT", "DNS"], key='-DROPDOWN-C-', font=('Consolas', 10))],
         [sg.Text("", size=(1, 1))],
         [sg.Button('Mostrar', font=('Consolas', 10))],
         [sg.Output(size=(80,10), font=('Consolas', 10), key="-OUTPUT-")],
@@ -161,56 +117,40 @@ def connection_router(ip, usuario, password):
     while True:
         event, values = window_configuration.read()
         if event == 'Mostrar':
-            if values['-DROPDOWN-'] == "Interfaces":
-                try:
-                    window_configuration['-OUTPUT-'].update("")
-                    print('Conectando ...')
-                    output = get_interfaces(ip, usuario, password)
-                    print(output)
-                except Exception as e:
-                    print('Error: ', str(e))
-            elif values['-DROPDOWN-'] == "Ruteo":
-                try:
-                    window_configuration['-OUTPUT-'].update("")
-                    print('Conectando ...')
-                    output = get_route(ip, usuario, password)
-                    print(output)
-                except Exception as e:
-                    print('Error: ', str(e))
-            elif values['-DROPDOWN-'] == "ACL":
-                try:
-                    window_configuration['-OUTPUT-'].update("")
-                    print('Conectando ...')
-                    output = get_lists(ip, usuario, password)
-                    print(output)
-                except Exception as e:
-                    print('Error: ', str(e))
-            elif values['-DROPDOWN-'] == "DHCP":
-                try:
-                    window_configuration['-OUTPUT-'].update("")
-                    print('Conectando ...')
-                    output = get_dhcp(ip, usuario, password)
-                    print(output)
-                except Exception as e:
-                    print('Error: ', str(e))
-            elif values['-DROPDOWN-'] == "NAT":
-                try:
-                    window_configuration['-OUTPUT-'].update("")
-                    print('Conectando ...')
-                    output = get_nat(ip, usuario, password)
-                    print(output)
-                except Exception as e:
-                    print('Error: ', str(e))
-            elif values['-DROPDOWN-'] == "DNS":
-                try:
-                    window_configuration['-OUTPUT-'].update("")
-                    print('Conectando ...')
-                    output = get_dns(ip, usuario, password)
-                    print(output)
-                except Exception as e:
-                    print('Error: ', str(e))
-            elif values['-DROPDOWN-'] == "":
-                sg.popup('Error', 'Seleccione una opcion')
+            # Comprobacion de que se selecciono algo
+            if values['-DROPDOWN-C-'] == "":
+                sg.popup('Error', 'Seleccione una configuracion')
+            elif values['-DROPDOWN-R-'] == "" and values['-DROPDOWN-C-'] == "":
+                sg.popup('Error', 'Seleccione un router y una configuracion')
+            
+            # Obtencion de la informacion
+            if values['-DROPDOWN-R-'] == "R1":
+                window_configuration['-OUTPUT-'].update("")
+                print('Conectando ...')
+                output = get_configuration('10.1.1.1', usuario, password, values['-DROPDOWN-C-'])
+                print(output)
+            elif values['-DROPDOWN-R-'] == "R2":
+                window_configuration['-OUTPUT-'].update("")
+                print('Conectando ...')
+                output = get_configuration('10.1.4.1', usuario, password, values['-DROPDOWN-C-'])
+                print(output)
+            elif values['-DROPDOWN-'] == "R3":
+                window_configuration['-OUTPUT-'].update("")
+                print('Conectando ...')
+                output = get_configuration('10.1.3.1', usuario, password, values['-DROPDOWN-C-'])
+                print(output)
+            elif values['-DROPDOWN-'] == "R4":
+                window_configuration['-OUTPUT-'].update("")
+                print('Conectando ...')
+                output = get_configuration('10.1.3.2', usuario, password, values['-DROPDOWN-C-'])
+                print(output)
+            elif values['-DROPDOWN-'] == "R5":
+                window_configuration['-OUTPUT-'].update("")
+                print('Conectando ...')
+                output = get_configuration('192.172.10.1', usuario, password, values['-DROPDOWN-C-'])
+                print(output)
+            elif values['-DROPDOWN-R-'] == "":
+                sg.popup('Error', 'Seleccione un router')
 
         if event == 'Regresar' or event == sg.WIN_CLOSED: # Definimos si se cierra la ventana se termina el evento o si se selecciona salir
             window_configuration.close()
@@ -218,6 +158,45 @@ def connection_router(ip, usuario, password):
             break
 
     window_configuration.close()
+
+# Funcion para retornar configuracion
+def get_configuration(ip, user, password, option):
+    if option == "Interfaces":
+        try:
+            output = get_interfaces(ip, user, password)
+            return output
+        except Exception as e:
+            return str(e)
+    elif option == "Ruteo":
+        try:
+            output = get_route(ip, user, password)
+            return output
+        except Exception as e:
+            return str(e)
+    elif option == "ACL":
+        try:
+            output = get_lists(ip, user, password)
+            return output
+        except Exception as e:
+            return str(e)
+    elif option == "DCHP":
+        try:
+            output = get_dhcp(ip, user, password)
+            return output
+        except Exception as e:
+            return str(e)
+    elif option == "NAT":
+        try:
+            output = get_nat(ip, user, password)
+            return output
+        except Exception as e:
+            return str(e)
+    elif option == "DNS":
+        try:
+            output = get_dns(ip, user, password)
+            return output
+        except Exception as e:
+            return str(e)
 
 # Funcion para obtener la informacion de ruteo
 def get_route(ip, user, password):
@@ -329,28 +308,6 @@ def get_dns(ip, user, password):
         shell.send('show running-config | include dns\n')
         time.sleep(1)
         shell.send('show run | section name-server\n')
-        time.sleep(5) 
-        output = shell.recv(65535).decode('ascii')
-        return "Conexion exitosa!\n"+output
-    except Exception as e:
-        return "Error en la conexion:\n"+str(e)
-
-# Funcion para obtener la informacion de VPN
-def get_vpn(ip, user, password):
-    try:
-        # Realizamos la configuracion de SSH
-        ssh_client = paramiko.SSHClient()
-        ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        ssh_client.connect(hostname=ip, port='22', username=user, password=password)
-
-        # Mandamos los comandos que queremos
-        shell = ssh_client.invoke_shell()
-        time.sleep(1)
-        shell.send('show interfaces tunnel 0\n')
-        time.sleep(1)
-        shell.send('show crypto session\n')
-        time.sleep(1)
-        shell.send('show crypto isakmp policy\n')
         time.sleep(5) 
         output = shell.recv(65535).decode('ascii')
         return "Conexion exitosa!\n"+output
